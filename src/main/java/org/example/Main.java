3c88f17a0fca9d0bb89c71aab62bda3537d1ed7a
Padma Priya <priyadhanasekar2006@gmail.com>
2026-06-18 12:41:26 +0530

    Added ArrayList, search methods and exception handling

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,86 @@
 package org.example;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        Book[] books = new Book[10];
-        Member[] members = new Member[5];
-        Loan[] loans = new Loan[5];
-        books[0] = new Book(1, "Harry Potter and The Philosopher's Stone", "J.K.Rowling", 750.00, BookStatus.AVAILABLE);
-        books[1] = new Book(2, "Pride and Prejudice", "Jane Austen", 250.00, BookStatus.AVAILABLE);
-        books[2] = new Book(3, "Fantastic Beasts and Where to Find Them", "J.K.Rowling", 450.00, BookStatus.BORROWED);
-        books[3] = new Book(4, "The Guide", "R.K.Narayanan", 300.00, BookStatus.AVAILABLE);
-        books[4] = new Book(5, "War and Peace", "Leo Tolstoy", 1050.00, BookStatus.RESERVED);
-        books[5] = new Book(6, "The Alchemist", "Paulo Coelho", 1985.00, BookStatus.AVAILABLE);
-        books[6] = new Book(7, "Hamlet", "William Shakespeare", 1200.00, BookStatus.AVAILABLE);
-        books[7] = new Book(8, "The Blue Umbrella ", "Ruskin Bond", 800.50, BookStatus.BORROWED);
-        books[8] = new Book(9, "Wonder", "R.J.Palacio", 560.00, BookStatus.AVAILABLE);
-        books[9] = new Book(10, "The Wild Robot", "Peter Brown", 400.00, BookStatus.RESERVED);
-        members[0] = new Member(1, "Hannah");
-        members[1] = new Member(2, "David");
-        members[2] = new Member(3, "Leoni");
-        members[3] = new Member(4, "Arun");
-        members[4] = new Member(5, "Priya");
-        loans[0] = new Loan(books[0], members[3]);
-        loans[1] = new Loan(books[3], members[0]);
-        loans[2] = new Loan(books[7], members[1]);
-        loans[3] = new Loan(books[9], members[4]);
-        loans[4] = new Loan(books[5], members[2]);
+        ArrayList<Book> books = new ArrayList<>();
+        ArrayList<Member> members = new ArrayList<>();
+        ArrayList<Loan> loans = new ArrayList<>();
+        books.add(new Book(1, "Harry Potter and The Philosopher's Stone", "J.K.Rowling", 750.00, BookStatus.AVAILABLE));
+        books.add(new Book(2, "Pride and Prejudice", "Jane Austen", 250.00, BookStatus.AVAILABLE));
+        books.add(new Book(3, "Fantastic Beasts and Where to Find Them", "J.K.Rowling", 450.00, BookStatus.BORROWED));
+        books.add(new Book(4, "The Guide", "R.K.Narayanan", 300.00, BookStatus.AVAILABLE));
+        books.add(new Book(5, "War and Peace", "Leo Tolstoy", 1050.00, BookStatus.RESERVED));
+        books.add(new Book(6, "The Alchemist", "Paulo Coelho", 1985.00, BookStatus.AVAILABLE));
+        books.add(new Book(7, "Hamlet", "William Shakespeare", 1200.00, BookStatus.AVAILABLE));
+        books.add(new Book(8, "The Blue Umbrella ", "Ruskin Bond", 800.50, BookStatus.BORROWED));
+        books.add(new Book(9, "Wonder", "R.J.Palacio", 560.00, BookStatus.AVAILABLE));
+        books.add(new Book(10, "The Wild Robot", "Peter Brown", 400.00, BookStatus.RESERVED));
+        members.add(new Member(1, "Hannah"));
+        members.add(new Member(2, "David"));
+        members.add(new Member(3, "Leoni"));
+        members.add(new Member(4, "Arun"));
+        members.add(new Member(5, "Priya"));
+        loans.add(new Loan(books.get(0), members.get(3)));
+        loans.add(new Loan(books.get(3), members.get(0)));
+        loans.add(new Loan(books.get(7), members.get(1)));
+        loans.add(new Loan(books.get(9), members.get(4)));
+        loans.add(new Loan(books.get(5), members.get(2)));
+        displayBooks(books);
+        displayMembers(members);
+        displayLoans(loans);
+        try{
+        Book book=searchBookbyId(books,11);
+        if (book==null) {
+            throw new Exception("Book not found");
+        }
+            System.out.println("Book Title:" +book.Title());
+            System.out.println("Author     :" +book.Author());
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        Member member=searchMemberbyId(members,2);
+        if(member!=null) {
+            System.out.println("\nMember Found");
+            System.out.println("Member Name :" +member.getName());
+        } else {
+            System.out.println("Member not found");
+        }
+    }
+    public static void displayBooks(ArrayList<Book> books){
+        System.out.println("\nBook Details\n");
         System.out.printf("%-5s %-50s %-30s %-10s %-10s%n", "Id", "Book Name", "Author", "Price","Status");
-        for (int i = 0; i < 10; i++) {
-            System.out.printf("%-5d %-50s %-30s %-10.2f %-10s%n", books[i].Id(), books[i].Title(), books[i].Author(), books[i].Price(),books[i].Status());
+        for(Book book:books) {
+            System.out.printf("%-5d %-50s %-30s %-10.2f %-10s%n", book.Id(), book.Title(), book.Author(), book.Price(),book.Status());
         }
+    }
+    public static void displayMembers(ArrayList<Member> members){
+        System.out.println("\nMember Details\n");
         System.out.printf("%-5s %-20s%n", "Member Id", "Name");
-        for (int i = 0; i < 5; i++) {
-            System.out.printf("%-5d %-20s%n", members[i].getMemberId(), members[i].getName());
+        for(Member member:members) {
+            System.out.printf("%-5d %-20s%n", member.getMemberId(), member.getName());
         }
+    }
+    public static void displayLoans(ArrayList<Loan> loans){
+        System.out.println("\nLoan Details");
         System.out.printf("%-5s %-20s %-50s %-20s %-20s", "Member Id", "Name", "Book Name", "Issue Date", "Due Date");
-        for (int i = 0; i < 5; i++) {
-            System.out.printf("%-10d %-20s %-50s %-20s %-20s%n", loans[i].getMemberId(), loans[i].getMember().getName(), loans[i].getBook().Title(), loans[i].getIssueDate(), loans[i].getDueDate());
+        for(Loan loan:loans) {
+            System.out.printf("%-10d %-20s %-50s %-20s %-20s%n", loan.getMemberId(), loan.getMember().getName(), loan.getBook().Title(), loan.getIssueDate(), loan.getDueDate());
         }
-    }}
+    }
+    public static Book searchBookbyId(ArrayList<Book> books,int id){
+        for (Book book : books) {
+            if (book.Id() == id) {
+                return book;
+            }
+        }
+        return null;
+    }
+    public static Member searchMemberbyId(ArrayList<Member>members,int id){
+        for(Member member:members){
+            if(member.getMemberId()==id){
+                return member;
+            }
+        }
+        return null;
+    }
+}
